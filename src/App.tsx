1e19b7dd57c005e64c79a04f91d0260f332672af
tranngoctu512004 <tranngoctu512004@gmail.com>
2024-11-03 15:49:54 +0700

    initial-comit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-2xl font-semibold text-gray-700 mb-4">Admin Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root container not found");
+}
+
+// Đo hiệu suất ứng dụng
+reportWebVitals(console.log);
